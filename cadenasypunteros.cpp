796df3d00bd831a73b1fb67be321494104aed836
San-43 <virgosmp.mx@gmail.com>
2022-10-14 22:30:31 -0500

    Hi

---
+++ cadenasypunteros.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <windows.h>
+#include <clocale>
+
+void prinSTR(char str[]);
+void posic(char str[]);
+void reverse(char str[]);
+
+using namespace std;
+
+int main() {
+    char str[50];
+    setlocale(LC_CTYPE, "spanish");
+    SetConsoleCP(1252); // Cambiar STDIN -  Para máquinas Windows
+    SetConsoleOutputCP(1252); // Cambiar STDOUT - Para máquinas Windows
+    system("color 0c");
+
+    cout<< "Introduzca una frase: "<< endl;
+    gets(str);
+
+
+    prinSTR(str);
+    system("pause");
+
+    posic(str);
+    system("pause");
+    reverse(str);
+    cout << endl << endl;
+    system("pause");
+
+    cout << "Gracias por usar mi programa";
+
+    return 0;
+}
+
+void prinSTR(char str[]) {
+    char *ptr = &str[0];
+    system("cls");
+
+    cout << "La frase es: "<<endl;
+    for (int i = 0; i < strlen(str); ++i) {
+        cout << *ptr << endl;
+        ptr++;
+    }
+}
+
+void reverse(char str[]) {
+    char *ptr = &str[strlen(str) - 1];
+    system("cls");
+
+    cout << "La frase al revés es: "<<endl;
+    for (int i = 0; i < strlen(str); ++i) {
+        cout << *ptr;
+        ptr--;
+    }
+}
+
+void posic(char str[]) {
+    char *ptr = &str[0];
+    system("cls");
+    vector<int> space;
+    vector<int> pto;
+    vector<int> pto_coma;
+    vector<int> coma;
+    int comac = 0, ptoc = 0, spacec= 0, pto_comac = 0;
+
+    for (int i = 0; i < strlen(str); ++i) {
+        if (*ptr == ' ') {
+            space.push_back(i);
+            spacec++;
+        } else if (*ptr == ',') {
+            coma.push_back(i);
+            comac++;
+        } else if (*ptr == '.') {
+            pto.push_back(i);
+            ptoc++;
+        } else if (*ptr == ';') {
+            pto_coma.push_back(i);
+            pto_comac++;
+        }
+        cout << *ptr;
+        ptr++;
+    }
+
+    cout << endl << endl;
+    cout << "Existen espacios en las posiciones: ";
+    for (int i: space) {
+        cout << i << ", ";
+    }
+    cout << ".En total son " << spacec << endl;
+
+    cout << endl << endl;
+    cout << "Existen puntos en las posiciones: ";
+    for (int i: pto) {
+        cout << i << ", ";
+    }
+    cout << ".En total son " << ptoc << endl;
+
+    cout << endl << endl;
+    cout << "Existen puntos y comas en las posiciones: ";
+    for (int i: pto_coma) {
+        cout << i << ", ";
+    }
+    cout << ".En total son " << pto_comac << endl;
+
+    cout << endl << endl;
+    cout << "Existen comas en las posiciones: ";
+    for (int i: coma) {
+        cout << i << ", ";
+    }
+    cout << ".En total son " << comac << endl;
+
+    cout << endl << endl;
+}
